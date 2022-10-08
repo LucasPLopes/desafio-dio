49213a065bba7087cbe6d2ff44c84d2e27e0b7ac
Lucas Lopes <l.lopes.fga@gmail.com>
2022-10-08 00:57:17 -0300

    update IConta and add logs operations

--- banco-digital/src/main/java/org/example/model/ContaCorrente.java
+++ banco-digital/src/main/java/org/example/model/ContaCorrente.java
@@ -2,10 +2,17 @@ package org.example.model;
 
 import org.example.interfaces.IConta;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ContaCorrente extends Conta implements IConta {
+    private List<String> logs = new ArrayList<>();
+
     public double sacar(double valor) {
-        double resultado =0;
-        if(getSaldo() >= valor){
+        logs.add(operacao("sacar",valor));
+        double resultado = 0;
+        if (getSaldo() >= valor) {
             resultado = getSaldo() - valor;
             setSaldo(resultado);
             return getSaldo();
@@ -14,13 +21,25 @@ public class ContaCorrente extends Conta implements IConta {
     }
 
     public double transferir(double valor, IConta iConta) {
+        logs.add(operacao("transferir",valor));
         double valorTransferred = sacar(valor);
         iConta.depositar(valorTransferred);
         return valorTransferred;
     }
 
     public double depositar(double valor) {
+        logs.add(operacao("depositar",valor));
         setSaldo(getSaldo() + valor);
         return getSaldo();
+    }
+
+    @Override
+    public String operacao(String descricao, Double valor) {
+        return String.format("%s - OP: %s - Valor: %d", LocalDateTime.now().toString(), descricao, valor);
+    }
+
+    @Override
+    public List<String> extrato() {
+        return logs;
     }
 }

844f9427ea46349f328707df23fb96bf31bef86c
Lucas Lopes <l.lopes.fga@gmail.com>
2022-10-08 01:11:58 -0300

    extract general IConta implementation to Conta and refactors

--- banco-digital/src/main/java/org/example/model/ContaCorrente.java
+++ banco-digital/src/main/java/org/example/model/ContaCorrente.java
@@ -1,45 +1,9 @@
 package org.example.model;
 
-import org.example.interfaces.IConta;
+public class ContaCorrente extends Conta {
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-
-public class ContaCorrente extends Conta implements IConta {
-    private List<String> logs = new ArrayList<>();
-
-    public double sacar(double valor) {
-        logs.add(operacao("sacar",valor));
-        double resultado = 0;
-        if (getSaldo() >= valor) {
-            resultado = getSaldo() - valor;
-            setSaldo(resultado);
-            return getSaldo();
-        }
-        return resultado;
-    }
-
-    public double transferir(double valor, IConta iConta) {
-        logs.add(operacao("transferir",valor));
-        double valorTransferred = sacar(valor);
-        iConta.depositar(valorTransferred);
-        return valorTransferred;
-    }
-
-    public double depositar(double valor) {
-        logs.add(operacao("depositar",valor));
-        setSaldo(getSaldo() + valor);
-        return getSaldo();
-    }
-
-    @Override
-    public String operacao(String descricao, Double valor) {
-        return String.format("%s - OP: %s - Valor: %d", LocalDateTime.now().toString(), descricao, valor);
-    }
-
-    @Override
-    public List<String> extrato() {
-        return logs;
+    public void extratoPretty() {
+        System.out.printf("Extract for CC: %d - AG: %d\n", super.getConta(), super.getAgencia());
+        super.extrato().forEach(System.out::println);
     }
 }

607b7898d946447a53def79877262eb26f66e6bb
Lucas Lopes <l.lopes.fga@gmail.com>
2022-10-08 19:58:26 -0300

    start project

---
+++ cloud-parking-dio/src/test/java/com/example/cloudparkingdio/CloudParkingDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudparkingdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudParkingDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
